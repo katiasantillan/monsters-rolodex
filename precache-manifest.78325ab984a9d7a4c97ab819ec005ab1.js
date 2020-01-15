7237b0100860277814538ade8bde81375b379035
Katia Santillan <kat_santillan@outlook.com>
2020-01-15 11:31:11 -0600

    Updates

---
+++ precache-manifest.78325ab984a9d7a4c97ab819ec005ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994281d958d0a06265ba806b95de0582",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1c1919eb8207cae97ff",
+    "url": "/monsters-rolodex/static/css/main.4d8064a9.chunk.css"
+  },
+  {
+    "revision": "fcdd128eef2034e2c8ea",
+    "url": "/monsters-rolodex/static/js/2.4fc5497d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.4fc5497d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1c1919eb8207cae97ff",
+    "url": "/monsters-rolodex/static/js/main.149af86a.chunk.js"
+  },
+  {
+    "revision": "1672be7e2e42305550d3",
+    "url": "/monsters-rolodex/static/js/runtime-main.8a2e9529.js"
+  }
+]);
